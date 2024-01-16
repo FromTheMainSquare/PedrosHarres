dd7eb408a7acc3f174c012477489cd1894ee9034
FromTheMainSquare <156787577+FromTheMainSquare@users.noreply.github.com>
2024-01-16 11:19:36 +0100

    Add files via upload

---
+++ img/js/pedroharres.js
@@ -0,0 +1,21 @@
+$(function() {
+	/*
+	$('article ul li a').click(function(){
+		
+		//console.log($(this).find('img').attr('data'));
+		//console.log($(this).closest('article').find('img').first().attr('src'));
+
+		var src=$(this).find('img').attr('rel');
+		var target=$(this).closest('article').find('img').first();
+
+		$(target).attr('src',src);
+
+		
+		return false;
+
+	});*/
+
+
+
+
+});
